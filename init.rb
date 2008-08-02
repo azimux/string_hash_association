cf4f58130029cc4e8d3c113d2ba9027495363ba0
Miles Georgi <azimux@gmail.com>
2008-08-02 02:25:31 -0700

    fixed some bugs and added functionality

--- init.rb
+++ init.rb
@@ -1,14 +1,16 @@
+require 'azimux/string_hash_association_proxy'
+
 ActiveRecord::Base.class_eval do
   def self.has_string_hash association_name, options = {}
     define_method association_name do
-      read_instance_variable("#{association_name}_string_hash_proxy") ||
-        write_instance_variable("#{association_name}_string_hash_proxy",
-        Azimux::StringHashAssociation.new(association_name, self, options))
+      instance_variable_get("@#{association_name}_string_hash_proxy") ||
+        instance_variable_set("@#{association_name}_string_hash_proxy",
+        Azimux::StringHashAssociationProxy.new(association_name, self, options))
     end
 
     method_name = "after_save_for_#{association_name}".to_sym
     define_method(method_name) do
-      association = read_instance_variable("#{association_name}_string_hash_proxy")
+      association = instance_variable_get("@#{association_name}_string_hash_proxy")
 
       association.save if !association.nil? && new_record?
     end
--- lib/azimux/string_hash_association_proxy.rb
+++ lib/azimux/string_hash_association_proxy.rb
@@ -1,13 +1,15 @@
 module Azimux
   class StringHashAssociationProxy
-    private_attr_accessor :target_class
-    private_attr_accessor :primary_key
+    attr_accessor :target_class
+    attr_accessor :primary_key
     attr_accessor :foreign_key_column
-    private_attr_accessor :key_column
-    private_attr_accessor :value_column
-    private_attr_accessor :owner
+    attr_accessor :key_column
+    attr_accessor :value_column
+    attr_accessor :owner
 
+    private :target_class, :primary_key, :key_column, :value_column, :owner
 
+public
     def initialize association_name, owner, options
       self.target_class = options[:class_name] || infer_target_class(association_name)
       self.primary_key = options[:primary_key] || owner.id
@@ -16,27 +18,44 @@ module Azimux
       self.value_column = options[:key] || 'value'
     end
 
+
     def [] key
       string_hash_cache[key] if owner.new_record?
 
       row ||= string_hash_cache[key]
 
       if row.nil?
-        row = string_hash_cache[key] ||= target_class.find(:first,
-          :conditions => ["#{key_column} = ? && #{foreign_key_column} = ?", key, primary_key])
+        row = string_hash_cache[key] ||= row_by_key
       end
 
       row && row.send(value_column)
     end
 
     def []= key, value
-      string_hash_cache[key] ||= target_class.find(:first, :conditions => ["#{key_column} = ?", key])
+      string_hash_cache[key] ||= row_by_key
 
       row ||= string_hash_cache[key] ||= target_class.new(key_column => key,
         value_column => value, foreign_key_column => primary_key)
 
       string_hash_cache[key].save unless new_record?
       value
+    end
+
+    def each_pair
+      string_hash_cache.fully_load
+
+      string_hash_cache.each_pair do |key,value|
+        yield key, value.send(value_column)
+      end
+    end
+
+    def row_by_key(key)
+      target_class.find(:first, :conditions => ["#{key_column} = ? && #{foreign_key_column} = ?", key, primary_key])
+    end
+
+    def clear
+      target_class.delete_all(["#{foreign_key_column} = ?", primary_key])
+      string_hash_cache.clear
     end
 
     def save
@@ -48,9 +67,30 @@ module Azimux
       end
     end
 
+
     private
     def string_hash_cache
       @string_hash_cache ||= {}
+    end
+    def fully_load
+      unless fully_loaded?
+        target_class.find(:first, :conditions => ["#{foreign_key_column} = ?", primary_key]).each do |row|
+          string_hash_cache[row.send(key_column)] ||= row
+        end
+
+        @fully_loaded = true
+      end
+    end
+
+    def fully_loaded?
+      @fully_loaded ||= false
+    end
+
+    def infer_target_class association_name
+      association_name.to_s.underscore
+    end
+    def infer_foreign_key_column owner
+      owner.class.to_s.foreign_key
     end
   end
 end
@@ -62,6 +102,16 @@ ActiveRecord::Base.class_eval do
       read_instance_variable("#{association_name}_string_hash_proxy") ||
         write_instance_variable("#{association_name}_string_hash_proxy",
         Azimux::StringHashAssociationProxy.new(association_name, self, options))
+    end
+
+    define_method "#{association_name}=" do |hash|
+      s_hash = self.send("#{association_name}_string_hash_proxy")
+      s_hash.clear
+
+      hash.each_pair do |key,value|
+        s_hash[key] = value
+      end
+      s_hash
     end
 
     method_name = "after_save_for_#{association_name}".to_sym
