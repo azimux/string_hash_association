976356a2fc51804c8aad5a8b808a93aa7d2e0678
Miles Georgi <azimux@gmail.com>
2009-03-10 13:22:43 -0700

    Fixed several bugs and made the code not string=>string dependent.

--- lib/azimux/hash_association_proxy.rb
+++ lib/azimux/hash_association_proxy.rb
@@ -1,5 +1,5 @@
 module Azimux
-  class StringHashAssociationProxy
+  class HashAssociationProxy
     attr_accessor :target_class
     attr_accessor :primary_key
     attr_accessor :foreign_key_column
@@ -9,8 +9,9 @@ module Azimux
 
     private :target_class, :primary_key, :key_column, :value_column, :owner
 
-public
+    public
     def initialize association_name, owner, options
+      self.owner = owner
       self.target_class = (options[:class_name] || infer_target_class(association_name)).constantize
       self.primary_key = options[:primary_key] || owner.id
       self.foreign_key_column = options[:foreign_key] || infer_foreign_key_column(owner)
@@ -18,33 +19,32 @@ public
       self.value_column = options[:key] || 'value'
     end
 
-
     def [] key
-      string_hash_cache[key] if owner.new_record?
+      g_hash_cache[key] if owner.new_record?
 
-      row ||= string_hash_cache[key]
+      row ||= g_hash_cache[key]
 
       if row.nil?
-        row = string_hash_cache[key] ||= row_by_key
+        row = g_hash_cache[key] ||= row_by_key(key)
       end
 
       row && row.send(value_column)
     end
 
     def []= key, value
-      string_hash_cache[key] ||= row_by_key
+      g_hash_cache[key] ||= row_by_key
 
-      row ||= string_hash_cache[key] ||= target_class.new(key_column => key,
+      row ||= g_hash_cache[key] ||= target_class.new(key_column => key,
         value_column => value, foreign_key_column => primary_key)
 
-      string_hash_cache[key].save unless new_record?
+      g_hash_cache[key].save unless new_record?
       value
     end
 
     def each_pair
-      string_hash_cache.fully_load
+      g_hash_cache.fully_load
 
-      string_hash_cache.each_pair do |key,value|
+      g_hash_cache.each_pair do |key,value|
         yield key, value.send(value_column)
       end
     end
@@ -55,11 +55,22 @@ public
 
     def clear
       target_class.delete_all(["#{foreign_key_column} = ?", primary_key])
-      string_hash_cache.clear
+      g_hash_cache.clear
+    end
+
+    def delete(key)
+      g_hash_cache.delete(key)
+      row = row_by_key(key)
+
+      if row
+        retval = row.send(value_column)
+        row.destroy
+        retval
+      end
     end
 
     def save
-      string_hash_cache.values.each do |value|
+      g_hash_cache.values.each do |value|
         if value.new_record? || value.send(association.foreign_key_column) != id
           value.send("#{foreign_key_column}=", id)
           value.save(true)
@@ -69,13 +80,13 @@ public
 
 
     private
-    def string_hash_cache
-      @string_hash_cache ||= {}
+    def g_hash_cache
+      @g_hash_cache ||= {}
     end
     def fully_load
       unless fully_loaded?
         target_class.find(:first, :conditions => ["#{foreign_key_column} = ?", primary_key]).each do |row|
-          string_hash_cache[row.send(key_column)] ||= row
+          g_hash_cache[row.send(key_column)] ||= row
         end
 
         @fully_loaded = true
@@ -86,26 +97,57 @@ public
       @fully_loaded ||= false
     end
 
-    def infer_target_class association_name
+    %w(infer_target_class
+      infer_foreign_key_column
+      infer_owner_table
+      infer_column_type
+    ).each do |m|
+      define_method m do |arg|
+        self.class.send(m, arg)
+      end
+    end
+
+    %w(col_types).each do |m|
+      define_method m do
+        self.class.send(m)
+      end
+    end
+
+
+    def self.infer_target_class association_name
       association_name.to_s.singularize.camelize
     end
-    def infer_foreign_key_column owner
+
+    def self.infer_foreign_key_column owner
       owner.class.to_s.foreign_key
+    end
+
+    def self.col_types
+      %w(integer string text boolean)
+    end
+    def self.infer_owner_table name
+      if name.to_s =~ /^(\S*)_(?:#{col_types.join("|")})_options$/
+        $1.pluralize
+      end
+    end
+    def self.infer_column_type name
+      if name.to_s =~ /^\S*_(#{col_types.join("|")})_options$/
+        $1
+      end
     end
   end
 end
 
-
 ActiveRecord::Base.class_eval do
-  def self.has_string_hash association_name, options = {}
+  def self.has_hash association_name, options = {}
     define_method association_name do
-      read_instance_variable("#{association_name}_string_hash_proxy") ||
-        write_instance_variable("#{association_name}_string_hash_proxy",
-        Azimux::StringHashAssociationProxy.new(association_name, self, options))
+      instance_variable_get("@#{association_name}_hash_proxy") ||
+        instance_variable_set("@#{association_name}_hash_proxy",
+        Azimux::HashAssociationProxy.new(association_name, self, options))
     end
 
     define_method "#{association_name}=" do |hash|
-      s_hash = self.send("#{association_name}_string_hash_proxy")
+      s_hash = self.send("#{association_name}_hash_proxy")
       s_hash.clear
 
       hash.each_pair do |key,value|
@@ -116,7 +158,7 @@ ActiveRecord::Base.class_eval do
 
     method_name = "after_save_for_#{association_name}".to_sym
     define_method(method_name) do
-      association = read_instance_variable("#{association_name}_string_hash_proxy")
+      association = instance_variable_get("@#{association_name}_hash_proxy")
 
       association.save if !association.nil? && new_record?
     end
---
+++ lib/azimux/hash_association_proxy/migration.rb
@@ -0,0 +1,32 @@
+module Azimux
+  class HashAssociationProxy
+    module Migration
+      def add_hash_association_table name, options = {}
+        owner = options[:owner] || HashAssociationProxy.infer_owner_table(name).singularize
+        type = options[:type] || HashAssociationProxy.infer_column_type(name)
+
+        ownerid = owner + "_id"
+
+        fk = nil
+
+        #if user explicitly set :owner_table to nil, then don't use a foreign key
+        unless options.include?(:owner_table) && !options[:owner_table]
+          fk = options[:owner_table] || HashAssociationProxy.infer_owner_table(name)
+        end
+
+        create_table name do |t|
+          t.string :name, :null => false
+          t.send(type, :value)
+          t.integer ownerid, :null => false, :deferrable => true,
+            :references => fk
+        end
+
+        add_index name, [ownerid, :name]
+      end
+
+      def remove_hash_association_table name
+        drop_table(name)
+      end
+    end
+  end
+end
